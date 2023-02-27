5516c56d8bb7d62eaa1ad32f3936f3d44756da61
l4pukhh <k0styalopuh3@gmail.com>
2023-02-27 20:37:09 +0200

    modules, path, fs, os, global variables

---
+++ app.js
@@ -0,0 +1,79 @@
+// -- MODULES --
+
+// const { sayHello } = require('./helper');
+//
+// sayHello();
+
+// -- GLOBAL VARIABLES --
+// const { logToConsole } = require('./test/test')
+//
+// console.log('FROM app.js');
+//
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(process.cwd());
+
+// -- PATH --
+// const path = require('path');
+
+// const joinedPath = path.join(__dirname, 'test', 'test.js');
+// console.log(joinedPath);
+
+// const normalizedPath = path.normalize('///test///test2/test.txt');
+// console.log(normalizedPath);
+
+// const resolvedPath = path.resolve('test', 'test.js');
+// console.log(resolvedPath);
+
+// -- OS --
+// const os = require('os');
+
+// console.log(os.arch());
+// console.log(os.cpus());
+
+// -- FS --
+const fs = require('fs');
+const path = require('path');
+
+// fs.writeFile(path.join('test', 'text2.txt'), 'Hello from Okten !!!', (err)=>{
+//   if (err) throw new Error(err.message)
+// })
+
+// fs.readFile(path.join('test', 'text.txt'), (err, data)=>{
+//   if (err) throw new Error(err.message);
+//   console.log(data.toString());
+// })
+
+// fs.appendFile(path.join('test', 'text2.txt'), '\nHello from Okten again!', (err)=>{
+//   if (err) throw new Error();
+// })
+
+// fs.truncate(path.join('test', 'text2.txt'), (err)=>{
+//   if (err) throw new Error();
+// })
+
+// fs.unlink(path.join('test', 'text2.txt'), (err)=>{
+//   if (err) throw new Error();
+// })
+
+// fs.stat(path.join('test'), (err, stats)=>{
+//   if (err) throw new Error();
+//   console.log(stats.isDirectory());
+//   console.log(stats.isFile());
+// })
+//
+// fs.readdir(path.join('test'), {withFileTypes: true},(err, data)=>{
+//   if (err) throw new Error();
+//   data.forEach(file=>{
+//     console.log(file.isFile());
+//   })
+// })
+// fs.mkdir(path.join('test', 'test2'), (err)=>{
+//   if (err) throw new Error();
+// })
+
+
+
+
+
+
